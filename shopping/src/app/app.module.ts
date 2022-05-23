59ea7c9455579a53993774c0e612bb1ca529a540
Lalo Flores <eflores@crudcreativo.com>
2022-05-23 16:27:54 -0400

    adding ingredients

--- shopping/src/app/app.module.ts
+++ shopping/src/app/app.module.ts
@@ -8,10 +8,11 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
@@ -21,8 +22,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipesListComponent,
     RecipesDetailComponent,
     RecipeItemComponent,
-    ShoppingListComponent,
     ShoppingEditComponent,
+    ShoppingListComponent,
     DropdownDirective
   ],
   imports: [
@@ -30,7 +31,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     AppRoutingModule,
     FormsModule
   ],
-  providers: [],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- shopping/src/app/recipes/recipe.service.ts
+++ shopping/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -8,10 +9,17 @@ export class RecipeService {
   recipes: Recipe[] = [
     new Recipe('A test',
     'Hola',
-    'https://www.cocinadominicana.com/wp-content/uploads/2010/12/croqueta-de-pollo-ClaraGon3245-600x900.jpg'),
+    'https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg', [
+      new Ingredient('Meat', 1),
+      new Ingredient('Meats',3)
+    ]),
     new Recipe('Un rico burger',
     'Burger',
-    'https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg')
+    'https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg',
+    [
+      new Ingredient('Ketchup', 10),
+      new Ingredient('Banana', 8)
+    ])
   ];
 
   getRecipes() {
